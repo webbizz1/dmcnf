f5f20558034d9224120960482027fd945d83966b
OhDongJoon <97786348+OHDongJoon@users.noreply.github.com>
2025-02-28 15:16:05 +0900

    Delete webbizz-crm-core directory

--- webbizz-crm-core/src/main/java/webbizz/crm/config/CacheConfig.java
+++
@@ -1,9 +0,0 @@
-package webbizz.crm.config;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class CacheConfig {
-}
