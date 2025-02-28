1d2506cd91555908051c65ed47f4bfe1e82d010a
dongjoon <kok3443@naver.com>
2025-02-28 14:14:06 +0900

    로그인 남은시간

--- webbizz-crm-web-admin/src/main/java/webbizz/crm/config/SessionController.java
+++ webbizz-crm-web-admin/src/main/java/webbizz/crm/config/SessionController.java
@@ -1,16 +1,24 @@
 package webbizz.crm.config;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
 
+@Aspect
+@Component
+@RequiredArgsConstructor
 @Slf4j
-@RestController
 public class SessionController {
 
-    @GetMapping("/session-time-left")
+
+    private final HttpSession session;
+
+    @PostMapping("/session-time-left")
     public long getSessionTimeLeft(HttpSession session) {
         long creationTime = session.getCreationTime(); // 세션이 생성된 시간 (밀리초)
         int maxInactiveInterval = session.getMaxInactiveInterval(); // 세션 유지 시간 (초)
@@ -23,5 +31,13 @@ public class SessionController {
             return 0; // 세션 만료
         }
         return timeLeft / 1000; // 초 단위 반환
+    }
+
+    @After("execution(@org.springframework.web.bind.annotation.GetMapping * *(..))") // @GetMapping이 적용된 메서드 실행 전
+    public void resetSession() {
+        if (session != null) {
+            session.setMaxInactiveInterval(3600); // 세션 유지 시간 60분 (3600초)으로 설정
+            log.info("세션");
+        }
     }
 }
