1e1e297a13662df668f6d10e81b84744e1243f17
hyunmii <hyunmiiyu@gmail.com>
2025-03-13 17:17:03 +0900

    탭버튼, 서브 페이지 ing

--- webbizz-crm-web-user/src/main/resources/static/js/sub.js
+++ webbizz-crm-web-user/src/main/resources/static/js/sub.js
@@ -1,24 +1,24 @@
 // 공통
 window.addEventListener('DOMContentLoaded', () => {
   function isMobileDevice() {
-		return window.innerWidth <= 1241;
-	}
+    return window.innerWidth <= 1241;
+  }
 
   // 공유 버튼 클릭 시 공유 팝업 표시/숨기기
-	// const shareBtn = document.querySelector('.function-wrap .btn-share');
+  // const shareBtn = document.querySelector('.function-wrap .btn-share');
   // const sharePopup = shareBtn.nextElementSibling;
-  
-	// shareBtn.addEventListener('click', function (e) {
+
+  // shareBtn.addEventListener('click', function (e) {
   //   e.stopPropagation();
-	// 	if(sharePopup.classList.contains('on')) {
-	// 		sharePopup.classList.remove('on');
-	// 	} else {
-	// 		sharePopup.classList.add('on');
-	// 	}
+  // 	if(sharePopup.classList.contains('on')) {
+  // 		sharePopup.classList.remove('on');
+  // 	} else {
+  // 		sharePopup.classList.add('on');
+  // 	}
   //   sharePopup.querySelector('.btn-close').addEventListener('click', function () {
   //     sharePopup.classList.remove('on');
   //   });
-	// });
+  // });
 
   // 외부 영역 클릭 시 모든 공유 팝업, custom select 등 닫기
   document.addEventListener('click', function (e) {
@@ -30,7 +30,7 @@ window.addEventListener('DOMContentLoaded', () => {
         hideAction();
       }
     }
-    
+
     // 공유 팝업 닫기
     // outsideClickClose('.function-wrap', function () {
     //   sharePopup.classList.remove('on');
@@ -38,7 +38,7 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   let tabBtn = document.querySelectorAll('.tab-wrap .tab');
-  tabBtn.forEach( tab => {
+  tabBtn.forEach(tab => {
     const tabSelect = tab.closest('.tab-wrap').querySelector('.tab-select');
     tabSelect.textContent = tab.closest('.tab-wrap').querySelector('.tab.on').textContent;
 
@@ -55,10 +55,10 @@ window.addEventListener('DOMContentLoaded', () => {
       tabSelect.classList.remove('on');
     } else {
       tabSelect.classList.add('on');
-      const tabWrap = tabSelect.closest('.tab-wrap').querySelector('.w-set');
+      const tabWrap = tabSelect.closest('.tab-wrap').querySelector('.tab-inner');
       tabWrap.style.height = tabWrap.querySelector('.select-list').offsetHeight + 'px';
     }
-    tabBtn.forEach( tab => {
+    tabBtn.forEach(tab => {
       tab.addEventListener('click', () => {
         tabSelect.nextElementSibling.style.height = '0';
         tabSelect.classList.remove('on');
@@ -67,12 +67,12 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  // document.querySelector('.tab-select').addEventListener('click', (e) => toggleTab(e));
+  document.querySelector('.tab-select').addEventListener('click', (e) => toggleTab(e));
 
   // 재단 정관
   document.querySelectorAll(".accordion-box .sub-header").forEach(header => {
     header.addEventListener("click", () => {
-      const accordion = header.closest(".accordion-box"); 
+      const accordion = header.closest(".accordion-box");
       const isActive = accordion.classList.contains("active");
 
       // 모든 아코디언 닫기
@@ -83,7 +83,7 @@ window.addEventListener('DOMContentLoaded', () => {
       if (!isActive) {
         accordion.classList.add("active");
         setTimeout(() => {
-          const yOffset = -100; 
+          const yOffset = -100;
           const y = accordion.getBoundingClientRect().top + window.scrollY + yOffset;
 
           window.scrollTo({ top: y, behavior: "smooth" });
